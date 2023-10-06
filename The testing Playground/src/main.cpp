55c51248c9367b2344f0527109907b5ecf6ab9e8
Marc Rathje <marcrathje12@gmail.com>
2023-10-06 09:25:58 +0200

    Addition of the default Platform IO Arduino code

---
+++ The testing Playground/src/main.cpp
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  Serial.print("Good morning!");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
